7751dfe073b54b01a91c8d8f7e60e5dde504fd6c
Tim <guoyong19890907@gmail.com>
2023-02-26 02:00:10 +0800

    v1.0.0打包上线

--- vite.config.js
+++ vite.config.js
@@ -45,5 +45,28 @@ export default defineConfig({
         }
       }
     }
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        }
+      }
+    }
   }
 })
